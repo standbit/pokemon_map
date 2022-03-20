4bf7d59434f05d1a75eebc422b56ecd2f76caecb
Stanislav <77130336+standbit@users.noreply.github.com>
2022-03-20 16:18:00 +0300

    Add datetime fields to PokemonEntity

--- pogomap/settings.py
+++ pogomap/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pokemon_entities'
+    'pokemon_entities',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,3 @@
-from distutils.command.upload import upload
-from email.mime import image
 from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
@@ -14,6 +12,8 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
+    appeared_at = models.DateTimeField()
+    disappeared_at = models.DateTimeField()
 
     def __str__(self):
         return f'Координаты {self.pokemon}а'
