c653d3cb9309ee01967fa1a39d9b540b0514503c
Stanislav <77130336+standbit@users.noreply.github.com>
2022-04-11 14:10:05 +0300

    Make pokemon entities fields required

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -36,33 +36,19 @@ class PokemonEntity(models.Model):
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(
-        verbose_name='Когда появился',
-        null=True,
-        blank=True)
+        verbose_name='Когда появился')
     disappeared_at = models.DateTimeField(
-        verbose_name='Когда исчезнет',
-        null=True,
-        blank=True)
+        verbose_name='Когда исчезнет')
     level = models.IntegerField(
-        verbose_name='Уровень',
-        null=True,
-        blank=True)
+        verbose_name='Уровень')
     health = models.IntegerField(
-        verbose_name='Здоровье',
-        null=True,
-        blank=True)
+        verbose_name='Здоровье')
     strength = models.IntegerField(
-        verbose_name='Атака',
-        null=True,
-        blank=True)
+        verbose_name='Атака')
     defence = models.IntegerField(
-        verbose_name='Защита',
-        null=True,
-        blank=True)
+        verbose_name='Защита')
     stamina = models.IntegerField(
-        verbose_name='Выносливость',
-        null=True,
-        blank=True)
+        verbose_name='Выносливость')
 
     def __str__(self):
         return f'{self.pokemon}'
